41a325be01f0df1df970f7d17c9ccb4ead717020
yasunori0418 <t-watanabe@gizumo-inc.jp>
2025-03-04 01:07:19 +0900

    add dependencies: kotest and mockk

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,13 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+    testImplementation("io.kotest:kotest-assertions-core:5.9.1")
+    testImplementation("io.kotest:kotest-property:5.9.1")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.3.0")
+    // testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:5.9.1")
+    // testImplementation("io.kotest.extensions:kotest-extensions-htmlreporter:5.9.1")
+    testImplementation("io.mockk:mockk:1.13.16")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
