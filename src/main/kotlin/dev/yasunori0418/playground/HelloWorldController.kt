e99e7a65c9b30b78509b5ccc017592ac4be19499
yasunori0418 <t-watanabe@gizumo-inc.jp>
2025-03-06 17:03:46 +0900

    style: formatted by ktlint

--- src/main/kotlin/dev/yasunori0418/playground/HelloWorldController.kt
+++ src/main/kotlin/dev/yasunori0418/playground/HelloWorldController.kt
@@ -1,12 +1,10 @@
 package dev.yasunori0418.playground
 
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloWorldController {
     @GetMapping("/")
-    fun helloWorld(): String {
-        return "Hello World!!"
-    }
+    fun helloWorld(): String = "Hello World!!"
 }
--- src/test/kotlin/dev/yasunori0418/playground/PlaygroundApplicationTests.kt
+++ src/test/kotlin/dev/yasunori0418/playground/PlaygroundApplicationTests.kt
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class PlaygroundApplicationTests {
-
     @Test
     fun contextLoads() {
     }
-
 }
